12c3a6b7cfd0d3b3e3ae127caf30679d8b4d9a01
pallavmukerjee1405 <81314117+pallavmukerjee1405@users.noreply.github.com>
2021-10-30 20:03:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ function preload(){
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
-    canvas=createCanvas(300,300);
-    canvas.center();
     video=createCapture(VIDEO);
     video.size(300,300);
     video.hide();
