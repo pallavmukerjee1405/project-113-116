cf8f273af5e9bc81c2bd985b0f1306c27e9f99c0
pallavmukerjee1405 <81314117+pallavmukerjee1405@users.noreply.github.com>
2021-11-06 20:15:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload(){
+    moustache_img=loadImage('https://i.postimg.cc/YqqmqJ1D/moustache-473661-1280.png');
 }
 
 function setup(){
@@ -19,14 +23,17 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        console.log("nose x ="+results[0].pose.nose.x);
-        console.log("nose y ="+results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x-7;
+        noseY=results[0].pose.nose.y-7;
+        console.log("nose x ="+noseX);
+        console.log("nose y ="+noseY);
     }
 }
 
 
 function draw(){
     image(video,0,0,300,300);
+    image(moustache_img,noseX,noseY,60,60);
 }
 
 function take_snapshot(){
