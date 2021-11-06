7e2b7d64cdd1440bd1b468274c1f5ab4cca9dc22
pallavmukerjee1405 <81314117+pallavmukerjee1405@users.noreply.github.com>
2021-11-06 20:17:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,8 +23,8 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        noseX=results[0].pose.nose.x-7;
-        noseY=results[0].pose.nose.y-7;
+        noseX=results[0].pose.nose.x-5;
+        noseY=results[0].pose.nose.y-5;
         console.log("nose x ="+noseX);
         console.log("nose y ="+noseY);
     }
